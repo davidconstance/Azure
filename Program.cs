7ac27427f0ebcd7dd22f234516d330fc3dc89cd6
David Constance <davidaleconstance@gmail.com>
2024-04-06 19:07:20 -0400

    Project DB

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace sqltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            { 
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "centralita-servidor.database.windows.net,1433";
+                builder.InitialCatalog = "centralita-base-de-datos";
+                builder.UserID = "dconstance";
+                builder.Password = "{.Dacd01092004}";
+                builder.Encrypt = true;
+                builder.TrustServerCertificate = false;
+                builder.MultipleActiveResultSets = false;
+                builder.PersistSecurityInfo = false;
+               
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+                    
+                    connection.Open();       
+
+                    String sql = "SELECT name, collation_name FROM sys.databases";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }                    
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nDone. Press enter.");
+            Console.ReadLine(); 
+        }
+    }
+}
